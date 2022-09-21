bad6e5f4cd477a61b6e500047d5ac15f60eaaadf
Nicholas Pedroso <nicholas.pedroso@vtex.com.br>
2022-09-20 21:53:14 -0300

    test: :sparkles: Add sonarqub properties

--- math.go
+++ math.go
@@ -9,3 +9,15 @@ func main() {
 func Soma(a int, b int) int {
 	return a + b
 }
+
+func Multiplicacao(a int, b int) int {
+	return a * b
+}
+
+func Divisao(a int, b int) int {
+	return a / b
+}
+
+func Subtracao(a int, b int) int {
+	return a - b
+}
